b1249f852bd45f194f7872447796cc7dde6210c2
aryan-palkar <76439351+aryan-palkar@users.noreply.github.com>
2022-03-05 14:19:44 +0530

    useState in login page

--- frontend/src/MyComponents/Login.jsx
+++ frontend/src/MyComponents/Login.jsx
@@ -1,35 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
 import profile from "../Images/loginimage/a.png";
 import email from "../Images/loginimage/email.jpg";
-import pass from "../Images/loginimage/pass.png"
+import pass from "../Images/loginimage/pass.png";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="main">
       <div className="sub-main">
-       <div>
-         <div className="imgs">
+        <div>
+          <div className="imgs">
             <div className="container-img">
-              <img src={profile} alt="profile" className="profile"/>
+              <img src={profile} alt="profile" className="profile" />
             </div>
-         </div>
-           <h2>Student Login</h2>
-           <div>
-             <img src={email} alt="email" className="email"/>
-              <input type="text" placeholder="username" className="name"/>
-           </div>
-           <div>
-             <img src={pass} alt="pass" className="email"/>
-              <input type="password" placeholder="password" className="name"/>
-           </div>
-           <div className="login-button">
-           <button >Login</button>
-           </div>
-          
-       </div>
+          </div>
+          <h2>Student Login</h2>
+          <div>
+            <img src={email} alt="email" className="email" />
+            <input
+              value={username}
+              onnChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              type="text"
+              placeholder="username"
+              className="name"
+            />
+          </div>
+          <div>
+            <img src={pass} alt="pass" className="email" />
+            <input
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              type="password"
+              placeholder="password"
+              className="name"
+            />
+          </div>
+          <div className="login-button">
+            <button>Login</button>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
