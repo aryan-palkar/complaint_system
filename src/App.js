e75b82e0a82051bf12906b558f94832ca8942388
pranshul15 <pranshul.hatwar@gmail.com>
2022-03-05 13:05:52 +0530

    Changes 5 March 13:05

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import DashBoard from './MyComponents/DashBoard';
 import Footer from './MyComponents/Footer';
 import Header from "./MyComponents/Header";
+import ImageSlider from './MyComponents/ImageSlider';
 import Informations from './MyComponents/Informations';
+import { SliderData } from './MyComponents/SliderData';
 
 function App() {
   return (
      <>
      <Header/>
      <DashBoard/>
+     <ImageSlider slides={SliderData}/>
      <Informations/>
      <Footer/>
      </>
---
+++ src/MyComponents/SliderData.js
@@ -0,0 +1,20 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1640622659208-98d70201f2da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1646309619580-07e862997c3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1646408271568-977e12b6425a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1646336197410-2535e4f0f3a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1646389950944-7fc4e53254ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1640622304964-3e2c2c0cd7cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    }
+]
